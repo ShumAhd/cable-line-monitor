2487b7fdded3397842e9265e00322c61c2a8110e
ShuMAhd <shuman@zykaika.ru>
2024-03-24 18:34:05 +0800

    first commit

---
+++ src/main/java/ru/shum/cablelinemonitor/CableLineMonitorApplication.java
@@ -0,0 +1,13 @@
+package ru.shum.cablelinemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CableLineMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CableLineMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shum/cablelinemonitor/CableLineMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shum.cablelinemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CableLineMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
